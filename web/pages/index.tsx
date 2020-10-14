960edc560ac82c182a901f693d78ce6c15f62b16
Luiz Pansarini <luiz.pansarini@uaubox.com.br>
2020-10-13 23:06:09 -0300

    first commit

---
+++ web/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Happy</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+       <h1>Hello NLW</h1>
+      </main>
+
+    </div>
+  )
+}
