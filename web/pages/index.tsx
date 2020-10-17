0b5bf3a7abbfe06f49d6a8a39c362bcbcdeda7bc
Luiz Henrique <lpansarini@gmail.com>
2020-10-17 00:23:57 -0300

    fix import to svg file

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import styled from "styled-components";
 
+import logoImg from "../public/logo.svg"
+
 const Container = styled.div`
+  background: linear-gradient(329.54deg, #29b6d1 0%, #00c7c7 100%);
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Logo = styled.img`
   background: linear-gradient(329.54deg, #29b6d1 0%, #00c7c7 100%);
   width: 100vw;
   height: 100vh;
@@ -16,7 +24,7 @@ export default function Home() {
       </Head>
 
       <Container>
-        <h1>Teste</h1>
+        <Logo src={logoImg} alt="Logo Happy" />
       </Container>
     </div>
   );
