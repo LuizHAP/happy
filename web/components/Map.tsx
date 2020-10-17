de3eb7fcfd0e19b725826f3e6167b21e94ba9d7d
Luiz Henrique <lpansarini@gmail.com>
2020-10-17 12:57:06 -0300

    add mapbox

--- web/components/Map.tsx
+++ web/components/Map.tsx
@@ -1,7 +1,5 @@
 import { Map, TileLayer } from "react-leaflet";
 
-i
-
 const MapWithNoSSR = () => {
   return (
     <Map
@@ -9,7 +7,10 @@ const MapWithNoSSR = () => {
       zoom={15}
       style={{ width: "100%", height: "100%" }}
     >
-      <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+      <TileLayer
+        url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.MAPBOX_TOKEN}`}
+      />
     </Map>
   );
 };
