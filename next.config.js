d9fcda069b45a3b5fd65beb4e51fb6a6d1794fbf
Luiz Henrique <lpansarini@gmail.com>
2020-10-28 17:39:16 -0300

    add next.config and change map

--- next.config.js
+++ next.config.js
@@ -1,2 +1,6 @@
-const withImages = require('next-images')
-module.exports = withImages()
+const withImages = require("next-images");
+module.exports = withImages({
+  env: {
+    MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
+  },
+});
